11987e4b5b0fbb8a23a842ae4718b0917362366c
Vinicius Josina <Viniciusburgie@gmail.com>
2025-02-04 21:25:42 -0300

    Conversao de int para Integer usando wrapper

--- PrimeiroProjeto/src/Main.java
+++ PrimeiroProjeto/src/Main.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World");
+        Scanner scanner = new Scanner(System.in);
 
+        int variavel;
 
+        System.out.println("Digite sua idade: ");
+        variavel = scanner.nextInt();
+
+        Integer variavelWrapper = Integer.valueOf(variavel);
+
+        System.out.println("Idade convertida = " + variavelWrapper);
     }
 }
